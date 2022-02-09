f7b0c5e6ae06a11cdf494e6d345271d39374e525
Mehmet <Mehmet.Kalich@hotmail.com>
2022-02-09 12:51:15 +0000

    Changes to be committed: 	modified:   apifetch_resume.js 	modified:   index.html 	modified:   mem.png

--- apifetch_resume.js
+++ apifetch_resume.js
@@ -1,3 +1,9 @@
+
+//Below a basic fetch request is used to fetch a response from the API's invoke link
+// and then push the JSON response of the status to the "count" variable in the html
+// in order to update the counter for the user + the backend. The code finishes
+// with both functions being invoked
+
 function getVisitorCount() {
   fetch(
     'https://1dmdyyqaz2.execute-api.us-east-1.amazonaws.com/counter-real/get-value'
